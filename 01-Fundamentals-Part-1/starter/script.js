fdae2d954b44ad0f9a69eb4b821b0dff66bae4d2
jadesolayo <oloyedeyetundejadesola@gmail.com>
2023-02-14 17:54:23 +0100

    created my script.js file and added some code to it

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,23 @@
+let js = "amazing";
+if (js === "amazing") alert("JavaScript is Fun!");
+
+
+/* Notes
+A value is a piece of data. It is the most fundamental unit of information that we  have in programming.
+
+Naming Variables .
+variable names cannot start with a number 
+variable names can only contain numbers , letters, underscores or the dolllar sign.
+Always write your variables with camel casing.
+Don't use reserved keywords as variable name e.g(name)
+Don't start a variable name with an Uppercase letter
+write constants with upper case later
+Make sure your variable names are descriptive(EASY TO UNDERSTAND).
+Do this:
+let myFirstJob = "Programmer";
+let mySecondJob = "Teacher";
+
+Don't do this
+job1 = "Teacher";
+job2 = "programmer";
+*/
