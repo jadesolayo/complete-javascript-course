b6cc29b8d7f21dfae9e27a55e0e244feb595cd1d
jadesolayo <oloyedeyetundejadesola@gmail.com>
2023-02-15 19:28:29 +0100

    modified script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,5 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is Fun!");
+// if (js === "amazing") alert("JavaScript is Fun!");
 
 let firstName = "Yetunde";
 let age = 23;
@@ -90,6 +90,25 @@ Typeof operator
 const lastName = "Schmedtmann";
 console.log(typeof lastName);
 
-
-
 */
+
+// Challenge 1
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both versions)
+3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+
+TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+
+GOOD LUCK 😀 */
+
+// Solution
+let BMI;
+let weightMark = 78;
+let heightMark = 1.69;
+BMI = weightMark / heightMark ** 2;
+console.log(BMI);
