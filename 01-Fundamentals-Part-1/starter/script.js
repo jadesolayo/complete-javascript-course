79f5737a6134e788aadebf8066a3319a9b5d069b
jadesolayo <oloyedeyetundejadesola@gmail.com>
2023-02-15 19:11:26 +0100

    added some code to my script file

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -43,11 +43,53 @@ Note that it is the value that holds the data type not the variable
 Let, Const and Var Variables
 
 Let and var variables can be redeclared i.e (mutable) but const cannot be redeclared i.e (immutable)
+
 let age = 30;
 age = 34;
 
 const P! = 3.141234
 
 var mybirthday = 'September'
-myBirthday = 'August 20
+myBirthday = 'August 20'
+
+Basic Operators in Javascript
+
+Math operator
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+Typeof operator
+
+const lastName = "Schmedtmann";
+console.log(typeof lastName);
+
+
+
 */
