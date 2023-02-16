9b525ee55a47a85902bceca4a9249586a6717295
jadesolayo <oloyedeyetundejadesola@gmail.com>
2023-02-16 19:28:00 +0100

    added more js codes

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -132,3 +132,11 @@ console.log(jonas);
 
 const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
 console.log(jonasNew);
+
+console.log(`Just a regular string....`);
+
+console.log("String with \n multiple \n lines");
+
+console.log(`String
+multiple
+lines`);
